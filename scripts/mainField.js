0308dae987fbb4daf30a81b0c3cc1bbdaf740d10
Leomar Linhares <leomar_linhares@hotmail.com>
2021-08-03 12:56:44 -0300

    ref: substitui do whiles por fors

--- scripts/mainField.js
+++ scripts/mainField.js
@@ -1,92 +1,109 @@
-var arrField = [];
-var usedFields = [];
-var globalLines = 0;
-var globalColumns = 0;
-var selectedBlock;
+let arrField = [];
+let usedFields = [];
+let globalLines = 0;
+let globalColumns = 0;
+let selectedBlock;
 
 function startGame() {
     let mainBox = document.querySelector('.mainBox');
 
     mainBox.innerHTML = '';
+    usedFields = [];
 
     let inputLines = document.querySelector('#lines').value;
     let inputColumns = document.querySelector('#columns').value;
 
-    let createdLines = 1;
-    let createdColumns = 1;
-
-    do {
+    for (let createdColumns = 0; createdColumns < inputColumns; createdColumns++) {
         let column = document.createElement('div');
-
+    
         column.setAttribute('class', 'column');
         column.setAttribute('id', `${createdColumns}`)
-        
-        do {
-            // System
 
-            var objectBlock = {};
-            objectBlock.coord = [createdColumns, createdLines];
+        for (let createdLines = 0; createdLines < inputLines; createdLines++) {
+            // System
+            let objectBlock = {};
+            objectBlock.coord = [createdColumns + 1, createdLines + 1];
             
             if (Math.random() * 100 < 20) {
                 objectBlock.isABomb = true;
             } else {
                 objectBlock.isABomb = false;
             }
-
+    
             arrField.push(objectBlock);
-
+    
             // Visual
-
             let lineBtn = document.createElement('button');
-
+    
             lineBtn.setAttribute('class', 'block');
-            lineBtn.setAttribute('id', `${createdColumns}-${createdLines}`);
-            lineBtn.setAttribute('onclick', `revealBlock(${createdColumns}, ${createdLines})`)
-
+            lineBtn.setAttribute('id', `${createdColumns + 1}-${createdLines + 1}`);
+            lineBtn.setAttribute('onclick', `revealBlock(${createdColumns + 1}, ${createdLines + 1})`)
+    
             column.appendChild(lineBtn)
-            createdLines++
-
-            globalLines = createdLines;
-
-        } while (inputLines >= createdLines)
-
+    
+            globalLines = createdLines + 1;
+        }
         mainBox.appendChild(column);
-        createdColumns++
-        createdLines = 1;
-
-        globalColumns = createdColumns;
-
-    } while (inputColumns >= createdColumns);
+    
+        globalColumns = createdColumns + 1;
+    }
 }
 
 function revealBlock(column, line) {
-    for (let index = 0; index < arrField.length; index++) {
-        const element = arrField[index];
-        const elCoord = element.coord;
-        
-        if (elCoord.toString() == [column, line].toString()) {
-            selectedBlock = element;
+    let isThisAUsedField = false;
+
+    for (let index = 0; index < usedFields.length; index++) {
+        const element = usedFields[index];
+        if (element === `${column}-${line}`) {
+            isThisAUsedField = true;
         }
     }
-    
-    let inScopeSelectedBlock = document.getElementById(`${selectedBlock.coord[0]}-${selectedBlock.coord[1]}`)
-    if (selectedBlock.isABomb) {
-        console.error('IS A BOMB!')
-        inScopeSelectedBlock.innerHTML = '*';
-        inScopeSelectedBlock.style.backgroundColor = 'red';
-        usedFields.push(`${column}-${line}`);
-    } else {
-        console.log(selectedBlock)
-        numberOfBombsAround(column, line)
+
+    if (!isThisAUsedField) {
+        for (let index = 0; index < arrField.length; index++) {
+            const element = arrField[index];
+            const elCoord = element.coord;
+            
+            if (elCoord.toString() == [column, line].toString()) {
+                selectedBlock = element;
+            }
+        }
+        
+        let inScopeSelectedBlock = document.getElementById(`${selectedBlock.coord[0]}-${selectedBlock.coord[1]}`)
+        if (selectedBlock.isABomb) {
+            console.error('IS A BOMB!')
+            inScopeSelectedBlock.innerHTML = '*';
+            inScopeSelectedBlock.style.backgroundColor = 'red';
+            usedFields.push(`${column}-${line}`);
+        } else {
+            console.log(selectedBlock)
+            numberOfBombsAround(column, line)
+        }
     }
 }
 
 function numberOfBombsAround(column, line) {
-    let isThisAUsedField = false;
-    let numberOfBombsAround = 0;
     let inScopeSelectedBlock = document.getElementById(`${selectedBlock.coord[0]}-${selectedBlock.coord[1]}`);
     let isInside = column > 0 && line > 0 && column <= globalColumns && line <= globalLines;
 
+    let responseHaveBombsAround = haveBombsAround(column, line);
+    let quantityOfBombs = responseHaveBombsAround.numberOfBombs;
+
+    if (isInside) {
+        usedFields.push(`${column}-${line}`);
+        inScopeSelectedBlock.innerHTML = quantityOfBombs;
+        inScopeSelectedBlock.style.backgroundColor = '#a9a9a9';
+    
+        if (!responseHaveBombsAround.numberOfBombs) {
+            noBombs(responseHaveBombsAround.fieldsAround);
+        }
+    }
+
+    return quantityOfBombs;
+}
+
+function haveBombsAround(column, line) {
+    let numberOfBombs = 0;
     let fieldsAround = {
         topLeft  : [column - 1, line - 1],
         top      : [column,     line - 1],
@@ -100,41 +117,30 @@ function numberOfBombsAround(column, line) {
         botRight : [column + 1, line + 1]
     }
 
-    for (let index = 0; index < usedFields.length; index++) {
-        const element = usedFields[index];
-        if (element === `${column}-${line}`) {
-            isThisAUsedField = true;
+    for (const key in fieldsAround) {
+        for (let index = 0; index < arrField.length; index++) {
+            const element = arrField[index];
+            if (element.coord.toString() === fieldsAround[key].toString() && element.isABomb) {
+                numberOfBombs += 1;
+            }
         }
     }
 
-    if (!isThisAUsedField && isInside) {
-        for (const key in fieldsAround) {
-            for (let index = 0; index < arrField.length; index++) {
-                const element = arrField[index];
-                if (element.coord.toString() === fieldsAround[key].toString() && element.isABomb) {
-                    numberOfBombsAround += 1;
-                }
-            }
-        }
-
-        usedFields.push(`${column}-${line}`);
-    
-        if (numberOfBombsAround === 0 ) {
-            for (const key in fieldsAround) {
-                inScopeSelectedBlock.innerHTML = numberOfBombsAround;
-                inScopeSelectedBlock.style.backgroundColor = '#a9a9a9';
-                noBombs(fieldsAround[key][0], fieldsAround[key][1], fieldsAround);
-            }
-        }
-
-        inScopeSelectedBlock.innerHTML = numberOfBombsAround;
-        inScopeSelectedBlock.style.backgroundColor = '#a9a9a9';
+    if (numberOfBombs > 0) {
+        return {
+            numberOfBombs,
+            fieldsAround
+        };
+    } else if (numberOfBombs === 0) {
+        return {
+            numberOfBombs,
+            fieldsAround
+        };
     }
 }
 
-function noBombs(column, line, receivedFields) {
+function noBombs(receivedFields) {
     for (const key in receivedFields) {
-        
-        numberOfBombsAround(column, line);
+        revealBlock(receivedFields[key][0], receivedFields[key][1]);
     }
 }
--- test/teste.js
+++ test/teste.js
@@ -1,17 +1,1 @@
-function numberOfBombsAround(column, line) {
-    let numberOfBombsAround = 0;
-
-    let topLeft  = [column--, line--]
-    let top      = [column, line--]
-    let topRight = [column++, line--]
-
-    let left     = [column--, line]
-    let right    = [column++, line]
-
-    let botLeft  = [column--, line++]
-    let bot      = [column, line++]
-    let botRight = [column++, line++]
-
-    
-}
-
+console.log(require('assert'))
